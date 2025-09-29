61090b6bc7642b4b4319dc56ee7533e37a09d899
Samantha Paez <sampaez@udel.edu>
2025-09-29 11:48:21 -0400

    get nest to work by changing password

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -10,7 +10,7 @@ import { AssignmentsService } from './assignments/assignments.service';
 import { PrismaModule } from './prisma.module';
 
 @Module({
-  imports: [LinksModule],
+  imports: [LinksModule, AssignmentsModule],
   controllers: [AppController],
   providers: [AppService,],
 })
