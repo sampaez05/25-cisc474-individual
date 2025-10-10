a5085c3d207ee53ad1bd4fd5ac010b26270ea99e
Samantha Paez <sampaez@udel.edu>
2025-10-10 10:10:40 -0400

    trying to fix prisma deployment error

--- apps/api/src/prisma.service.ts
+++ apps/api/src/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@repo/database/generated/client';
+import { PrismaClient } from '@repo/database';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
